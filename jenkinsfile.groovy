befbfb466f8068a0bc07d438f136004a1cd2ca8a
Bechir Ferjani <Bechir_ferjani@yahoo.fr>
2024-05-11 08:28:40 -0700

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,80 +3,23 @@ pipeline {
     
     // Define tools used in the pipeline
     tools {
-        // Define Maven tool with version 3.6.3
-        maven 'Maven'
-        nodejs 'NodeJS'
-        jdk 'Java'
     }
     
     environment {
-        scannerHome = tool 'Java'
+        scannerHome = tool 'Sonar';
     }
     
     stages {
-        stage('Run Backend Unit Tests') {
-            steps {
-                dir('spring-blog-backend') {
-                    sh 'mvn clean test'
-                }
-            }
-        }
 
-        stage('Build Backend') {
-            steps {
-                dir('spring-blog-backend') {
-                    sh 'mvn clean install'
-                }
-            }
-        }
-        
-        stage('Build Frontend') {
-            steps {
-                dir('spring-blog-client') {
-                    sh 'npm install'
-                    sh 'npm run build -- --configuration=production'
-                }
-            }
-        }
-        
-        stage('Run Frontend Unit Tests') {
-            steps {
-                dir('spring-blog-client') {
-                  //  sh 'npm run test'
-                }
-            }
-        }
-        
         stage('Code Analysis') {
             steps {
                 script {
-                    withCredentials([string(credentialsId: 'Sonarqube-token', variable: 'SONAR_LOGIN')]) {
-                        withSonarQubeEnv('SonarQubeServer') {
-                            sh """
-                                ${scannerHome}/bin/sonar-scanner \
-                                -Dsonar.projectKey=tunartisan \
-                                -Dsonar.java.binaries=. \
-                                -Dsonar.host.url=http://192.168.74.139:9010 \
-                                -Dsonar.login=squ_1ca99673dccd74a3038f8b7c456368bba9b4d85b
-                            """
-                        }
+                        sh ' ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=tunartisan_client_front -Dsonar.java.binaries=. -Dsonar.host.url=http://master:9000 -Dsonar.login=squ_a918939eb419f31cbdd274d2d03830fa34d3ee8c'
                     }
                 }
             }
         }
         
-        stage('Deploy Artifacts to Nexus') {
-            steps {
-                dir('spring-blog-backend/target') {
-                    //  Publish backend artifact to Nexus
-                    sh 'curl -v -u admin:nexus --upload-file spring-blog-backend-0.0.1-SNAPSHOT.jar http://192.168.74.134:8081/repository/maven-releases/'
-                }
-                
-                dir('spring-blog-client') {
-                    // Publish frontend artifact to Nexus (assuming Angular produces static files)
-                    sh 'curl -v -u admin:nexus --upload-file * http://192.168.74.134:8081/repository/npm-releases/'
-                }
-            }
-        }
+
     }
 }
