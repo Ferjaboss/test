943946bef5f487c589e8eb84cdd88cb38efa8475
Bechir Ferjani <Bechir_ferjani@yahoo.fr>
2024-05-11 08:37:20 -0700

    updated jenkins file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage('Code Analysis') {
             steps {
                 script {
-                        sh ' ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=tunartisan_client_front -Dsonar.java.binaries=. -Dsonar.host.url=http://master:9000 -Dsonar.login=squ_a918939eb419f31cbdd274d2d03830fa34d3ee8c'
+                        sh ' ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=fatma -Dsonar.java.binaries=. -Dsonar.host.url=http://master:9000 -Dsonar.login=squ_a918939eb419f31cbdd274d2d03830fa34d3ee8c'
                     }
                 }
             }
